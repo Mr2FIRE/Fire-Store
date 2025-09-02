4c2abf9f8c929916b4dcf7dd3816f2203079ef61
Mr2FIRE <130065624+Mr2FIRE@users.noreply.github.com>
2025-09-02 12:03:25 +0300

    Add ApproveFireForMarketplace component to Buy page

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -2,10 +2,12 @@
 export const dynamic = "force-dynamic";
 
 import MarketTab from "../components/MarketTab";
+import ApproveFireForMarketplace from "../components/ApproveFireForMarketplace";
 
 export default function Buy() {
 	return (
 		<div className="min-h-screen bg-gradient-to-br from-gray-900 to-black text-white px-4 py-8">
+			<ApproveFireForMarketplace />
 			<MarketTab />
 		</div>
 	);
